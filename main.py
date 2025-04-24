44a1c177e89dd35c6b4a0886eeab81e154ba6bd2
VIPABH <a891983.2@gmail.com>
2025-04-25 01:06:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ print('''
 to_config += f"\ntoken = '{token}'"
 to_config += f"\nDev_Zaid = token.split(':')[0]"
 to_config += f"\nsudo_id = {owner_id}"
-username = "kkajdjwjbot"
+username = requests.get(f"https://api.telegram.org/bot{token}/getMe").json()["result"]["username"]
 to_config += f"\nbotUsername = '{username}'"
 to_config += "\nfrom kvsqlite.sync import Client as DB"
 to_config += "\nytdb = DB('ytdb.sqlite')"
@@ -58,8 +58,10 @@ with open('config.py','w+') as w:
 print('''
 50% 
 ███████▒▒▒ ''')
-app = Client(f'{Dev_Zaid}r3d', 9398500, 'ad2977d673006bed6e5007d953301e13',
-  bot_token=token,
+API_ID = os.getenv("API_ID")  # من ملف .env
+API_HASH = os.getenv("API_HASH")  # من ملف .env
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # من ملف .env
+app = Client(f'{Dev_Zaid}r3d', API_ID, API_HASH, bot_token=BOT_TOKEN)
     plugins={"root": "Plugins"},
   )
 # userbot = Client('userbott', 9398500, "ad2977d673006bed6e5007d953301e13", session_string="BACPaOQAw9EWMijb1D8m_wYGIa2r6tnaNiJDVTuC4jVktrtF5K7UxjNuZNcA-HpmEBltGr-0rUrELER9Vj0CmkNb28BdGYGETl5dJIg386wdjv3ZYNB3HkYrbhN5GFE4w2tYNv5dQJmvLTtvC3bTa0HoW64YLPINX_3BEZSoyXPm_bbXonA_2PIqeA1MHdEzfg_U4Zy75xyBq0pBvTv6xhD9hpAliXHnapJ5gg4C8Qt4QX4JLMGYxaSTNt51OClNVpPU6yiKZBFYl-t6CP66VmL3JU3P3HshrCSlcY38GfZ7Uy_w1b7HCqqe9EnVmZV0k3S29YtFlGz9Z0uuw0pxloAFpebeTwAAAABydGQqAA")
