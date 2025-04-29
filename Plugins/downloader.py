8fdb4fb28ac06f89da07b1c83b1e67dc996a2832
VIPABH <a891983.2@gmail.com>
2025-04-30 00:36:26 +0300

    Update downloader.py

--- Plugins/downloader.py
+++ Plugins/downloader.py
@@ -1,4 +1,4 @@
-import yt_dlp,os, requests, re, time, wget, random, json 
+import yt_dlp, os, requests, re, time, wget, random, json 
 from yt_dlp import YoutubeDL
 from pytube import YouTube
 from youtube_search import YoutubeSearch as Y88F8
@@ -11,97 +11,104 @@ from config import *
 from helpers.Ranks import *
 from helpers.Ranks import isLockCommand
 from PIL import Image, ImageFilter
+
 shazam = Shazam()
+
 def time_to_seconds(time):
     stringt = str(time)
     return sum(
         int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":")))
     )
+
 def Find(text):
-  m = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:'\".,<>?«»“”‘’]))"
-  url = re.findall(m,text)  
-  return [x[0] for x in url]
+    m = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(m, text)  
+    return [x[0] for x in url]
+
 @Client.on_message(filters.text & filters.group, group=32)
-def ytdownloaderHandler(c,m):
+def ytdownloaderHandler(c, m):
     k = r.get(f'{Dev_Zaid}:botkey')
     channel = r.get(f'{Dev_Zaid}:BotChannel') if r.get(f'{Dev_Zaid}:BotChannel') else 'w7G_BoT'
-    Thread(target=yt_func,args=(c,m,k,channel)).start()
-def yt_func(c,m,k,channel):
-   if not r.get(f'{m.chat.id}:enable:{Dev_Zaid}'):
+    Thread(target=yt_func, args=(c, m, k, channel)).start()
+
+def yt_func(c, m, k, channel):
+    if not r.get(f'{m.chat.id}:enable:{Dev_Zaid}'):
         return False 
-   if r.get(f'{m.from_user.id}:mute:{m.chat.id}{Dev_Zaid}'):  return False
-   if r.get(f'{m.chat.id}:mute:{Dev_Zaid}') and not admin_pls(m.from_user.id,m.chat.id):  return False 
-   if r.get(f'{m.from_user.id}:mute:{Dev_Zaid}'):  return False 
-   text = m.text
-   if isLockCommand(m.from_user.id, m.chat.id, text): return
-   rep = InlineKeyboardMarkup (
-     [[
-       InlineKeyboardButton ('🧚‍♀️', url=f'https://t.me/{channel}')
-     ]]
-   )
-if text.startswith('بحث ') or text.startswith('yt '):
-    # إذا كانت خاصية تعطيل البحث مفعلة
-    # if r.get(f'{m.chat.id}:disableYT:{Dev_Zaid}') or r.get(f':disableYT:{Dev_Zaid}'):  
-        # return
-    
-    # استخراج الاستعلام من النص
-    query = text.split(None, 1)[1]
-    
-    # البحث في Y88F8
-    results = Y88F8(query, max_results=1).to_dict()
-    
-    # التأكد من وجود نتائج
-    # if results:
-    #     res = results[0]
-    # else:
-    #     return m.reply("لم يتم العثور على نتائج.")
-    
-    # التحقق من وجود الفيديو في قاعدة البيانات
-    if ytdb.get(f'ytvideo{res["id"]}'):
-        aud = ytdb.get(f'ytvideo{res["id"]}')
-        duration_string = time.strftime('%M:%S', time.gmtime(aud["duration"]))
-        m.reply_audio(
-            aud["audio"],
-            caption=f'@{channel} ~ {duration_string} ⏳',
-            reply_markup=rep
-        )
-        # return m.reply_audio(
-        #     aud["audio"],
-        #     caption=f'@{channel} ~ {duration_string} ⏳',
-        #     reply_markup=rep
-        # )
-    
-    url = f'https://youtu.be/{res["id"]}'
-    cc = 1 
-    print(url, cc)
-    yt = YouTube(url)
-    duration_string = time.strftime('%M:%S', time.gmtime(yt.length))
-    ydl_ops = {
-        "format": "bestaudio[ext=m4a]",
-        'forceduration': True,
-        "username": "oauth2",  # تحقق من البيانات الخاصة باليوزر
-        "password": ''  # تحقق من وجود كلمة المرور إذا كانت ضرورية
-    }
-    with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-          info = ydl.extract_info(url, download=False)
-          audio_file = ydl.prepare_filename(info)
-          ydl.process_info(info)
-          thumb = wget.download(yt.thumbnail_url)
-          os.rename(audio_file, audio_file.replace(".m4a", ".mp3"))
-          audio_file = audio_file.replace(".m4a", ".mp3")
-          a = m.reply_audio(
-              audio_file,
-              title=yt.title,
-              thumb=thumb,
-              duration=yt.length,
-              caption=f'@{channel} ~ {duration_string} ⏳',
-              performer=yt.author,
-              reply_markup=rep
+
+    if r.get(f'{m.from_user.id}:mute:{m.chat.id}{Dev_Zaid}'):  
+        return False
+
+    if r.get(f'{m.chat.id}:mute:{Dev_Zaid}') and not admin_pls(m.from_user.id, m.chat.id):  
+        return False 
+
+    if r.get(f'{m.from_user.id}:mute:{Dev_Zaid}'):  
+        return False 
+
+    text = m.text
+    if isLockCommand(m.from_user.id, m.chat.id, text): 
+        return
+
+    rep = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🧚‍♀️', url=f'https://t.me/{channel}')
+        ]]
+    )
+
+    if text.startswith('بحث ') or text.startswith('yt '):
+        # استخراج الاستعلام من النص
+        query = text.split(None, 1)[1]
+
+        # البحث في Y88F8
+        results = Y88F8(query, max_results=1).to_dict()
+        
+        if results:
+            res = results[0]
+        else:
+            return m.reply("لم يتم العثور على نتائج.")
+
+        # التحقق من وجود الفيديو في قاعدة البيانات
+        if ytdb.get(f'ytvideo{res["id"]}'):
+            aud = ytdb.get(f'ytvideo{res["id"]}')
+            duration_string = time.strftime('%M:%S', time.gmtime(aud["duration"]))
+            m.reply_audio(
+                aud["audio"],
+                caption=f'@{channel} ~ {duration_string} ⏳',
+                reply_markup=rep
             )
-          ytdb.set(f'ytvideo{res["id"]}', {
+            return
+        
+        url = f'https://youtu.be/{res["id"]}'
+        yt = YouTube(url)
+        duration_string = time.strftime('%M:%S', time.gmtime(yt.length))
+        ydl_ops = {
+            "format": "bestaudio[ext=m4a]",
+            'forceduration': True,
+            "username": "oauth2",  # تحقق من البيانات الخاصة باليوزر
+            "password": ''  # تحقق من وجود كلمة المرور إذا كانت ضرورية
+        }
+
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info = ydl.extract_info(url, download=False)
+            audio_file = ydl.prepare_filename(info)
+            ydl.process_info(info)
+            thumb = wget.download(yt.thumbnail_url)
+            os.rename(audio_file, audio_file.replace(".m4a", ".mp3"))
+            audio_file = audio_file.replace(".m4a", ".mp3")
+            a = m.reply_audio(
+                audio_file,
+                title=yt.title,
+                thumb=thumb,
+                duration=yt.length,
+                caption=f'@{channel} ~ {duration_string} ⏳',
+                performer=yt.author,
+                reply_markup=rep
+            )
+
+            ytdb.set(f'ytvideo{res["id"]}', {
                 "type": "audio",
                 "audio": a.audio.file_id,
                 "duration": a.audio.duration
             })
-          os.remove(audi_file)
-          os.remove(thum)
+            os.remove(audio_file)
+            os.remove(thumb)
+            return
