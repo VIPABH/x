1fafc5d3a9038363dd90c04f166ba8cf4bcb04f1
VIPABH <a891983.2@gmail.com>
2026-09-04 10:08:21 +0300

    Update id.py

--- Plugins/id.py
+++ Plugins/id.py
@@ -898,7 +898,7 @@ def get_my_rank(c,m,k):
 𖡋 𝐄𝐃𝐈𝐓 ⌯  {التعديل}
 𖡋 𝐂𝐑  ⌯  {الانشاء}
 {البايو}'''
-      if m.from_user.usernames:
+      if m.from_user.username:
          username = ''
          for i in m.from_user.usernames: username += f"@{i.username} "
       elif m.from_user.username:
@@ -1021,6 +1021,5 @@ def set_id(c,m,k):
        text = r.get(f'Custom:{Dev_Zaid}&text={text}')
 
 '''
-
 
 
