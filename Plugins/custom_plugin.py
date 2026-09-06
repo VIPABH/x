e3d326beb413dd23b2f4344327434a22a6c971b4
VIPABH <a891983.2@gmail.com>
2026-09-06 11:48:04 +0300

    Update custom_plugin.py

--- Plugins/custom_plugin.py
+++ Plugins/custom_plugin.py
@@ -1,167 +1,166 @@
-
-
-import random, re, time
+import yt_dlp, os, re, time, wget, json
+from youtube_search import YoutubeSearch as Y88F8
 from threading import Thread
-from pyrogram import *
+from pyrogram import Client, filters
 from pyrogram.enums import *
-from pyrogram.types import *
+from shazamio import Shazam
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import *
 from helpers.Ranks import *
+from helpers.Ranks import isLockCommand
 
+shazam = Shazam()
 
-@Client.on_message(filters.text & filters.group, group=31)
-def addPluginHandler(c,m):
+channel = r.get(f'{Dev_Zaid}botname') if r.get(f'{Dev_Zaid}botname') else 'x04ou'
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+
+def Find(text):
+    m = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(m, text)
+    return [x[0] for x in url]
+
+@Client.on_message(filters.text & filters.group, group=32)
+def ytdownloaderHandler(c, m):
     k = r.get(f'{Dev_Zaid}:botkey')
-    Thread(target=plugin_func,args=(c,m,k)).start()
-    
-def plugin_func(c,m,k):
-   if not r.get(f'{m.chat.id}:enable:{Dev_Zaid}'):
+    channel = r.get(f'{Dev_Zaid}:BotChannel') if r.get(f'{Dev_Zaid}:BotChannel') else 'w7G_BoT'
+    Thread(target=yt_func, args=(c, m, k, channel)).start()
+
+def yt_func(c, m, k, channel):
+
+    if not r.get(f'{m.chat.id}:enable:{Dev_Zaid}'):
+        return False
+
+    if r.get(f'{m.from_user.id}:mute:{m.chat.id}{Dev_Zaid}'):
+        return False
+
+    if r.get(f'{m.chat.id}:mute:{Dev_Zaid}') and not admin_pls(m.from_user.id, m.chat.id):
+        return False
+
+    if r.get(f'{m.from_user.id}:mute:{Dev_Zaid}'):
+        return False
+
+    text = m.text
+    if isLockCommand(m.from_user.id, m.chat.id, text):
         return
-   if r.get(f'{m.from_user.id}:mute:{m.chat.id}{Dev_Zaid}'):  return 
-   if r.get(f'{m.chat.id}:mute:{Dev_Zaid}') and not admin_pls(m.from_user.id,m.chat.id):  return
-   if r.get(f'{m.from_user.id}:mute:{Dev_Zaid}'):  return 
-   
-   if r.get(f'{m.chat.id}addCustomG:{m.from_user.id}{Dev_Zaid}'):  return
-   if r.get(f'{m.chat.id}:addCustom:{m.from_user.id}{Dev_Zaid}'):  return 
-   if r.get(f'{m.chat.id}:delCustom:{m.from_user.id}{Dev_Zaid}') or r.get(f'{m.chat.id}:delCustomG:{m.from_user.id}{Dev_Zaid}'):  return 
-   text = m.text
-   name = r.get(f'{Dev_Zaid}:BotName') if r.get(f'{Dev_Zaid}:BotName') else 'أكس گارد'
-   if text.startswith(f'{name} '):
-      text = text.replace(f'{name} ','')
-   if r.get(f'{m.chat.id}:Custom:{m.chat.id}{Dev_Zaid}&text={text}'):
-       text = r.get(f'{m.chat.id}:Custom:{m.chat.id}{Dev_Zaid}&text={text}')
-   if r.get(f'Custom:{Dev_Zaid}&text={text}'):
-       text = r.get(f'Custom:{Dev_Zaid}&text={text}')
-   
-   if r.get(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}') or r.get(f'{m.from_user.id}:setAddP:{m.chat.id}{Dev_Zaid}') or r.get(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}') or r.get(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}') or r.get(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}') or r.get(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}'):
-     if text == 'الغاء':
-       m.reply(f'{k} ابشر ياعيني لغيت كلشي')
-       r.delete(f'{m.from_user.id}:setAddP:{m.chat.id}{Dev_Zaid}')
-       r.delete(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}')
-       r.delete(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}')
-       r.delete(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}')
-       r.delete(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}')
-       return 
-     
-   if text == 'اضف ميزة' or text == 'اضف ميزه':
-     if devp_pls(m.from_user.id,m.chat.id):
-        r.set(f'{m.from_user.id}:setAddP:{m.chat.id}{Dev_Zaid}',1)
-        return m.reply(f'{k} هلا عيني ارسل اسم الميزة الحين')
-   
-   if r.get(f'{m.from_user.id}:setAddP:{m.chat.id}{Dev_Zaid}') and devp_pls(m.from_user.id,m.chat.id) and len(m.text.split()) == 1:
-      r.delete(f'{m.from_user.id}:setAddP:{m.chat.id}{Dev_Zaid}')
-      r.set(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}',m.text)
-      return m.reply(f'{k} تمام عيني ارسل نوع الميزة الحين ( صوره,فيديو,متحركه,بصمه,صوت)\n☆')
-   
-   if text in ['صوره','فيديو','متحركه','بصمه','صوت'] and r.get(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}') and devp_pls(m.from_user.id,m.chat.id):
-      miza = r.get(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}')
-      r.delete(f'{m.from_user.id}:setAddP2:{m.chat.id}{Dev_Zaid}')
-      r.set(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}',f'miza={miza}&&type={m.text}')
-      return m.reply(f'{k} ارسل يوزر القناة الحين')
-   
-   if r.get(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}') and devp_pls(m.from_user.id,m.chat.id):
-      miza = r.get(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}')
-      miza += f'&&channel={m.text.replace("@","")}'
-      r.delete(f'{m.from_user.id}:setAddP3:{m.chat.id}{Dev_Zaid}')
-      r.set(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}', miza)
-      return m.reply(f'{k} ارسل الحين ايديات الرسايل العشوائية\n{k} مثال 1 - 100')
-   
-   if r.get(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}') and devp_pls(m.from_user.id,m.chat.id):
-      miza = r.get(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}')
-      id1 = int(m.text.split('-')[0])
-      id2 = int(m.text.split('-')[1])
-      r.delete(f'{m.from_user.id}:setAddP4:{m.chat.id}{Dev_Zaid}')
-      miza_name = miza.split('miza=')[1].split('&&')[0]
-      miza_type = miza.split('&&type=')[1].split('&&')[0]
-      miza_channel = miza.split('&&channel=')[1].split('&&')[0]
-      r.set(f'{miza_name}:customPlugin:{Dev_Zaid}', f'type={miza_type}&&channel={miza_channel}&&random={id1}_{id2}')
-      r.sadd(f'customPlugins:{Dev_Zaid}', miza_name)
-      return m.reply(f'{k} ابشر ضفت الميزة ( {miza_name} )\n{k} نوع الميزة {miza_type}\n{k} قناة الميزة ( @{miza_channel} )')
-   
-   if text == 'مسح ميزة' or text == 'مسح ميزه':
-     if devp_pls(m.from_user.id,m.chat.id):
-        r.set(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}',1)
-        return m.reply(f'{k} هلا عيني ارسل اسم الميزة الحين')
-        
-   if r.get(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}') and devp_pls(m.from_user.id,m.chat.id):
-     if not r.get(f'{m.text}:customPlugin:{Dev_Zaid}'):
-       r.delete(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}')
-       return m.reply(f'{k} مافي ميزة بهالأسم')
-     else:
-       r.srem(f'customPlugins:{Dev_Zaid}', m.text)
-       r.delete(f'{m.text}:customPlugin:{Dev_Zaid}')
-       r.delete(f'{m.from_user.id}:setDelp:{m.chat.id}{Dev_Zaid}')
-       r.delete(f'{m.text}:customPluginD:{Dev_Zaid}{m.chat.id}')
-       return m.reply(f'{k} الميزة ( {m.text} ) مسحتها .')
-   
-   if text == 'المميزات المضافه':
-     if devp_pls(m.from_user.id,m.chat.id):
-       if not r.smembers(f'customPlugins:{Dev_Zaid}'):
-         return m.reply(f'{k} مافي ولا ميزة مضافة')
-       else:
-         text = 'المميزات المضافه:\n\n'
-         count = 1
-         for miza in r.smembers(f'customPlugins:{Dev_Zaid}'):
-            text += f'{count}) - {miza}\n'
-            count += 1
-         text += '\n☆'
-         return m.reply(text)
-   
-   if r.get(f'{m.text}:customPlugin:{Dev_Zaid}'):
-      if r.get(f'{m.text}:customPluginD:{Dev_Zaid}{m.chat.id}'):
-         return
-      else:
-         miza = r.get(f'{m.text}:customPlugin:{Dev_Zaid}')
-         type = miza.split('type=')[1].split('&&')[0]
-         channel = miza.split('&&channel=')[1].split('&&')[0]
-         random1 = int(miza.split('&&random=')[1].split('_')[0])
-         random2 = int(miza.split('&&random=')[1].split('_')[1])
-         rand = random.randint(random1,random2)
-         if type == 'صوره':
-            m.reply_photo(f'https://t.me/{channel}/{rand}')
-         
-         if type == 'فيديو':
-            m.reply_video(f'https://t.me/{channel}/{rand}')
-        
-         if type == 'متحركه':
-            m.reply_animation(f'https://t.me/{channel}/{rand}')
-         
-         if type == 'بصمه':
-            m.reply_voice(f'https://t.me/{channel}/{rand}')
-         
-         if type == 'صوت':
-            m.reply_audio(f'https://t.me/{channel}/{rand}')
-   
-   if text.startswith('تعطيل ') and len(text.split()) == 2:
-      miza = text.split()[1]
-      if r.get(f'{miza}:customPlugin:{Dev_Zaid}'):
-        if not owner_pls(m.from_user.id,m.chat.id):
-          return m.reply(f'{k} هذا الامر يخص ( المالك وفوق ) بس') 
-        else:
-          if r.get(f'{miza}:customPluginD:{Dev_Zaid}{m.chat.id}'):
-            return m.reply(f'{k} من「 {m.from_user.mention} 」\n{k} ميزة {miza} معطله من قبل\n☆')
-          else:
-            r.set(f'{miza}:customPluginD:{Dev_Zaid}{m.chat.id}',1)
-            return m.reply(f'من「 {m.from_user.mention} 」\n{k} ابشر عطلت ميزة {miza}\n☆')
-   
-   if text.startswith('تفعيل ') and len(text.split()) == 2:
-      miza = text.split()[1]
-      if r.get(f'{miza}:customPlugin:{Dev_Zaid}'):
-        if not owner_pls(m.from_user.id,m.chat.id):
-          return m.reply(f'{k} هذا الامر يخص ( المالك وفوق ) بس') 
-        else:
-          if not r.get(f'{miza}:customPluginD:{Dev_Zaid}{m.chat.id}'):
-            return m.reply(f'{k} من「 {m.from_user.mention} 」\n{k} ميزة {miza} مفعله من قبل\n☆')
-          else:
-            r.delete(f'{miza}:customPluginD:{Dev_Zaid}{m.chat.id}')
-            return m.reply(f'من「 {m.from_user.mention} 」\n{k} ابشر فعلت ميزة {miza}\n☆')
-   
-            
-            
-          
-   
-   
-   
-   
+
+    rep = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🧚‍♀️', url=f'https://t.me/{channel}')
+        ]]
+    )
+    if text.startswith('بحث'):
+     if r.get(f'{m.chat.id}:disableYT:{Dev_Zaid}'):  return
+     if r.get(f':disableYT:{Dev_Zaid}'):  return
+     query = text.split(None,1)[1]
+     keyboard= []
+     results=Y88F8(query,max_results=4).to_dict()
+     for res in results:
+       title = res['title']
+       id = res['id']
+       keyboard.append([InlineKeyboardButton (title, callback_data=f'{m.from_user.id}GET{id}')])     
+     a = m.reply(f'{k} البحث ~ {query}',reply_markup=InlineKeyboardMarkup (keyboard), disable_web_page_preview=True)
+     r.set(f'{a.id}:one_minute:{m.from_user.id}', 1, ex=60)
+     return True
+
+    if text.startswith('يوت') or text.startswith('yt '):
+      parts = text.split(None, 1)
+      if len(parts) < 2:
+          return m.reply("يرجى إدخال اسم الأغنية أو الرابط بعد الأمر.")
       
-   
+      query = parts[1]
+
+      results = Y88F8(query, max_results=1).to_dict()
+
+      if not results:
+          return m.reply("لم يتم العثور على نتائج.")
+
+      res = results[0]
+      video_id = res["id"]
+
+      cached_audio = ytdb.get(f'ytvideo{video_id}')
+      if cached_audio:
+          duration_string = time.strftime('%M:%S', time.gmtime(cached_audio.get("duration", 0)))
+          return m.reply_audio(
+              cached_audio["audio"],
+              caption=f'@{channel} ~ {duration_string} ⏳',
+              reply_markup=rep
+          )
+
+      url = f'https://youtu.be/{video_id}'
+      ydl_ops = {
+        'format': 'bestaudio/best',
+        'cookiefile': 'cookies.txt',
+        'remote_components': ['ejs:github'],
+        'nocheckcertificate': True,
+        'quiet': True,
+        'no_warnings': True,
+        'forceduration': True,
+        'noplaylist': True,
+        'outtmpl': f'downloads/{video_id}.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+      audio_file = None
+      thumb_file = None
+
+      try:
+          with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+              info = ydl.extract_info(url, download=True)
+              title = info.get('title', 'Audio')
+              duration = int(info.get('duration', 0))
+              thumbnail = info.get('thumbnail')
+              uploader = info.get('uploader', 'YouTube')
+              
+              # تحديد مسار ملف الـ MP3 الناتج بدقة
+              downloaded_file = ydl.prepare_filename(info)
+              audio_file = os.path.splitext(downloaded_file)[0] + ".mp3"
+
+          # التحقق من وجود الملف على القرص قبل المتابعة
+          if not os.path.exists(audio_file):
+              return m.reply("❌ تعذر العثور على الملف الصوتي بعد التحميل.")
+
+          duration_string = time.strftime('%M:%S', time.gmtime(duration))
+
+          # تحميل الصورة المصغرة إن وجدت
+          if thumbnail:
+              try:
+                  thumb_file = wget.download(thumbnail, out=f"downloads/thumb_{video_id}.jpg")
+              except Exception:
+                  thumb_file = None
+
+          # 3. إرسال الصوت لليوزر
+          sent_audio = m.reply_audio(
+              audio_file,
+              title=title,
+              thumb=thumb_file,
+              duration=duration,
+              caption=f'**enjoy dear** ~ {duration_string} ⏳',
+              performer=uploader,
+              reply_markup=rep
+          )
+
+          # 4. الحفظ في قاعدة البيانات
+          if sent_audio and getattr(sent_audio, 'audio', None):
+              ytdb.set(f'ytvideo{video_id}', {
+                  "type": "audio",
+                  "audio": sent_audio.audio.file_id,
+                  "duration": sent_audio.audio.duration
+              })
+
+      except Exception as e:
+          print(f"حدث خطأ أثناء تحميل الفيديو: {e}")
+          m.reply("حدث خطأ أثناء التحميل، يرجى المحاولة لاحقًا.")
+
+      finally:
+          # 5. تنظيف الملفات المؤقتة
+          if audio_file and os.path.exists(audio_file):
+              os.remove(audio_file)
+          if thumb_file and os.path.exists(thumb_file):
+              os.remove(thumb_file)
