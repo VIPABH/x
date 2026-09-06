25b3d6dfb3412450775ea40245d9264f1b81db61
VIPABH <a891983.2@gmail.com>
2026-09-06 20:35:22 +0300

    Update all.py

--- Plugins/all.py
+++ Plugins/all.py
@@ -2266,7 +2266,7 @@ def guardCommands(c, m, k, channel):
                 get = m.chat.get_member(user)
                 if m.from_user.id == get.user.id:
                     return m.reply("شفيك تبي تنزل نفسك")
-               rank = _get_rank(get.user.id, m.chat.id)
+                rank = _get_rank(get.user.id, m.chat.id)
                 if rank:
                     return m.reply(f"{k} هييه مايمديك تطرد {rank} ياورع!")
                 if get.status == ChatMemberStatus.BANNED:
