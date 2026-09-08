706851720badb028cd5489bdfe990b6035bfce0e
VIPABH <a891983.2@gmail.com>
2026-09-08 15:28:30 +0300

    Update downloader.py

--- Plugins/downloader.py
+++ Plugins/downloader.py
@@ -101,6 +101,7 @@ def yt_func(c, m, k, channel):
               audio_file = os.path.splitext(downloaded_file)[0] + ".mp3"
           if not os.path.exists(audio_file):
               return m.reply("❌ تعذر العثور على الملف الصوتي بعد التحميل.")
+          msg.edit('تم التحميل , يجري الرفع')
           duration_string = time.strftime('%M:%S', time.gmtime(duration))
           if thumbnail:
               try:
@@ -116,8 +117,8 @@ def yt_func(c, m, k, channel):
               performer=uploader,
               reply_markup=rep
           )
-          # his_name = m.from_user.mention
-          # sent_audio.reply(f"بحثك جهز عزيزي ( {his_name} )")
+        #   his_name = m.from_user.mention
+        #   sent_audio.reply(f"بحثك جهز عزيزي ( {his_name} )")
           link = f'https://t.me/c/{str(m.chat.id).replace("-100", "")}/{sent_audio.id}'
           msg.edit(f"تم التحميل ( [الرابط]({link}) )")
           if sent_audio and getattr(sent_audio, 'audio', None):
