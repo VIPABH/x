1a40f7b6357d1583a90fc2f923f9f312ca733741
VIPABH <a891983.2@gmail.com>
2026-09-10 11:57:07 +0300

    Update all.py

--- Plugins/all.py
+++ Plugins/all.py
@@ -95,7 +95,7 @@ def Find(text):
 async def on_zbi(c: Client, m: Message):
     name = r.get(f"{Dev_Zaid}:BotName") if r.get(f"{Dev_Zaid}:BotName") else "X"
     text = m.text
-    if text.startswith(f"{name} "):
+    if text and text.startswith(f"{name} "):
         text = text.replace(f"{name} ", "")
     if r.get(f"{m.chat.id}:Custom:{m.chat.id}{Dev_Zaid}&text={text}"):
         text = r.get(f"{m.chat.id}:Custom:{m.chat.id}{Dev_Zaid}&text={text}")
@@ -231,7 +231,7 @@ def guardResponseFunction(c, m, k, channel):
         if rep.document:
             file_id = rep.document.file_id
         else:
-            pass
+            return
         idd = file_id[-6:]
         if r.get(f"{idd}:NotAllow:{m.chat.id}{Dev_Zaid}"):
             if not admin_pls(id, m.chat.id):
